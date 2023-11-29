5f05a902b7d62789e33a3c4b6903fa13da0b9945
ImanAdithya <adithyaiman07@gmail.com>
2023-11-29 20:52:45 +0530

    content updated

--- second_project/lib/main.dart
+++ second_project/lib/main.dart
@@ -1,19 +1,59 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
+
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home:HomePage() ,
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("THIS IS TITLE"),
-        ),
+
+          appBar: AppBar(
+            title: Text("HOME"),
+
+            leading: const IconButton(
+              icon: Icon(Icons.menu,size: 40.0,color: Colors.white,),
+              onPressed:null,//onPressed: ()=> funtion(),
+            ),
+
+            actions: const [
+               IconButton(
+                   onPressed: null,
+                   icon: Icon(Icons.search,size: 30.0,color: Colors.white,)
+               ),
+
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.more_vert,size: 30.0,color: Colors.white,)
+              ),
+            ],
+
+          ),
+
+
       ),
     );
   }
-
 }
