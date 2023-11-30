2d88cc5779ac82497059354b22f9cd7e42620db8
ImanAdithya <adithyaiman07@gmail.com>
2023-11-30 08:32:17 +0530

    add TabBar view with content using method

--- third_project/lib/main.dart
+++ third_project/lib/main.dart
@@ -33,39 +33,46 @@ class HomePage extends StatelessWidget {
         home: Scaffold(
 
           appBar: AppBar(
-              title: const Text("HOME",style: TextStyle(color: Colors.black,fontSize: 25.0,fontWeight: FontWeight.bold)),
+              title: const Text("HOME",style: TextStyle(color: Colors.white,fontSize: 25.0,fontWeight: FontWeight.bold)),
 
               leading: const IconButton(
-                icon: Icon(Icons.menu,size: 40.0,color: Colors.black,),
+                icon: Icon(Icons.menu,size: 40.0,color: Colors.white,),
                 onPressed:null,//onPressed: ()=> funtion(),
               ),
 
               actions: const [
                 IconButton(
                     onPressed: null,
-                    icon: Icon(Icons.search,size: 30.0,color: Colors.black,)
+                    icon: Icon(Icons.search,size: 30.0,color: Colors.white,)
                 ),
 
                 IconButton(
                     onPressed: null,
-                    icon: Icon(Icons.more_vert,size: 30.0,color: Colors.black,)
+                    icon: Icon(Icons.more_vert,size: 30.0,color: Colors.white,)
                 ),
               ],
 
               flexibleSpace: Image.asset(
-                "assets/back.jpg",
+                "assets/redBack.jpg",
                 fit: BoxFit.cover,
               ),
 
               bottom:const TabBar(
                 tabs: [
-                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_car,)),
                   Tab(icon: Icon(Icons.directions_transit)),
                   Tab(icon: Icon(Icons.directions_bike)),
                 ],
               ),
-
-
+            
+          ),
+          
+          body: TabBarView(
+            children: [
+              tab1(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
           ),
 
 
@@ -73,4 +80,13 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+
+Widget tab1(){
+  return Container(
+    child: const Center(
+      child: Text("TAB 1 TEXT"),
+    ),
+  );
 }
