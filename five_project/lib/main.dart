4d28ec8e78be00f4b5669a2383c027bdf143adca
ImanAdithya <adithyaiman07@gmail.com>
2023-11-30 10:56:49 +0530

    project four

--- five_project/lib/main.dart
+++ five_project/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.dark,
         useMaterial3: true,
       ),
       home: MyHomePage(),
@@ -27,7 +27,33 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
+   return Scaffold(
+
+     appBar: AppBar(
+       backgroundColor: Colors.red,
+       title: Text("COLUMN AND ROW"),
+     ),
+
+     body: Container(
+       height: 300.0,
+       color: Colors.pink,
+       child: const Row(
+         children: [
+           Text("Text 1",style: TextStyle(fontSize: 20.0),),
+           Text("Text 2",style: TextStyle(fontSize: 20.0),),
+           Text("Text 3",style: TextStyle(fontSize: 20.0),),
+           Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Text 1",style: TextStyle(fontSize: 20.0),),
+              Text("Text 2",style: TextStyle(fontSize: 20.0),),
+              Text("Text 3",style: TextStyle(fontSize: 20.0),),
+            ],
+           )
+         ],
+       ),
+     ),
+
 
    );
   }
